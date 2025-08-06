dfeffdcbe85dc729371ee2b442ceec1137cc4828
Avulamanohar <125465328+Avulamanohar@users.noreply.github.com>
2025-08-06 08:07:55 +0530

    Time: 20 ms (5.47%), Space: 46 MB (6.41%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,52 @@
+class Solution {
+    public int romanToInt(String s)
+     {
+         List<Integer> arr=new ArrayList<>();
+            List<String> arr1=new ArrayList<>();
+            arr.add(1000);
+            arr.add(900);
+            arr.add(500);
+            arr.add(400);
+            arr.add(100);
+            arr.add(90);
+            arr.add(50);
+            arr.add(40);
+            arr.add(10);
+            arr.add(9);
+            arr.add(5);
+            arr.add(4);
+            arr.add(1);
+            arr1.add("M");
+            arr1.add("CM");
+            arr1.add("D");
+            arr1.add("CD");
+            arr1.add("C");
+            arr1.add("XC");
+            arr1.add("L");
+            arr1.add("XL");
+            arr1.add("X");
+            arr1.add("IX");
+            arr1.add("V");
+            arr1.add("IV");
+            arr1.add("I");
+            int i;
+            int n=arr.size();
+            int count=0;
+            
+          
+            for(i=0;i<n;i++)
+            {
+               
+               while(s.startsWith(arr1.get(i)))
+               {
+                count+=arr.get(i);
+                s=s.replaceFirst(arr1.get(i),"");
+             
+               }
+            }
+            return count;
+
+
+        
+    }
+}
