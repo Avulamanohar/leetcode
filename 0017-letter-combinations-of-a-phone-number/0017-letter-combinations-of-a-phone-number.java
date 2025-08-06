3b8707fb8f0ac9d686ce5048a84086c41c9da9b9
Avulamanohar <125465328+Avulamanohar@users.noreply.github.com>
2025-08-06 18:25:12 +0530

    Time: 0 ms (100%), Space: 41.6 MB (99.21%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,15 +1,15 @@
 class Solution {
     public List<String> letterCombinations(String digits)
     {
-        Map<Character,String> hs=Map.of(
-            '2',"abc",
-            '3',"def",
-            '4',"ghi",
-            '5',"jkl",
-            '6',"mno",
-            '7',"pqrs",
-            '8',"tuv",
-            '9',"wxyz"
+        Map<Integer,String> hs=Map.of(
+            2,"abc",
+            3,"def",
+            4,"ghi",
+            5,"jkl",
+            6,"mno",
+            7,"pqrs",
+            8,"tuv",
+            9,"wxyz"
             );
         List<String> ar=new ArrayList<>();
 
@@ -25,7 +25,7 @@ class Solution {
         
         
     }
-    public void phone(int Index,Map<Character,String> hs,String b,List<String> ar,StringBuilder sb)
+    public void phone(int Index,Map<Integer,String> hs,String b,List<String> ar,StringBuilder sb)
     {  
          int i,j;
         int n=b.length();
@@ -36,7 +36,7 @@ class Solution {
         }
         
        
-        char num=b.charAt(Index);
+        int num=b.charAt(Index)-'0';
         String s=hs.get(num);
         int Str=s.length();
       
