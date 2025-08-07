be930495dcb3427b61c0a432c2fab5a0fb24edfa
Avulamanohar <125465328+Avulamanohar@users.noreply.github.com>
2025-08-07 19:16:15 +0530

    Time: 1 ms (6.96%), Space: 41.6 MB (18.79%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.java
@@ -1,60 +1,63 @@
 import java.util.*;
 class Solution {
     public boolean check(int[] nums) 
-    {
-          ArrayList<Integer> arr=new ArrayList<Integer>();
-          ArrayList<Integer> arr1=new ArrayList<Integer>();
-    int n=nums.length;
-    int i;
-    int j=-1;
-    boolean b=true;
-    for(i=0;i<n;i++)
-    {
-      arr.add(nums[i]);
-    }    
-    for(i=0;i<n-1;i++)
-    {
-           if(arr.get(i)>arr.get(i+1))
-           {
-            j=i+1;
+    {   int i,j;
+        int n=nums.length;
+        boolean b=true;
+        int k=-1;
+        List<Integer> arr=new ArrayList<>();
+        for(int p:nums)
+        {
+            arr.add(p);
+        }
+
+      
+        
+        for(i=0;i<n-1;i++)
+        {
+            if(arr.get(i)>arr.get(i+1))
+            {
+                k=i+1;
              
-             break;
+              break;
+            }
+
+        }
+        if(k!=-1)
+        {
             
-           }
+        
+        List<Integer> arr1=new ArrayList<>( cheker(arr,k));
 
-    } 
-    if(j!=-1)
-    {
-        ch(arr1,arr,j);
+
+         for(i=0;i<n-1;i++)
+        {
+            if(arr1.get(i)>arr1.get(i+1))
+            {
+             b=false;
+             break;
+            }
+
+        }
+        }
+        
+        return b;
     }
-          
-         
-         int m=arr1.size();
-          for(i=0;i<m-1;i++)
-          {
-              if(arr1.get(i)>arr1.get(i+1))
-              {
-                b=false;
-                break;
-              }
-          }
-          return b;
-  
-
-
-    }
-    public ArrayList<Integer> ch(ArrayList<Integer> arr1,ArrayList<Integer> arr,int j)
+    public List<Integer> cheker(List<Integer> arr,int b )
     {
-             int n=arr.size();
-             int i;
-             for(i=j;i<n;i++)
-             {
-                arr1.add(arr.get(i));
-             }
-             for(i=0;i<j;i++)
-             {
-                arr1.add(arr.get(i));
-             }
-             return arr1;
+        List<Integer> arr1=new ArrayList<>();
+        int i,j;
+        int n=arr.size();
+        for(i=b;i<n;i++)
+        {
+            arr1.add(arr.get(i));
+    }
+    for(i=0;i<b;i++)
+    {
+        arr1.add(arr.get(i));
+    }
+   return arr1;
+
+       
     }
 }
