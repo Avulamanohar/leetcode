9966c94c5dfe0661054b1463c1fecc9f89aa9dd0
Avulamanohar <125465328+Avulamanohar@users.noreply.github.com>
2025-08-24 11:49:40 +0530

    Time: 7 ms (60.1%), Space: 44.6 MB (72.81%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -8,63 +8,40 @@
  * }
  */
 class Solution {
+    TreeNode r[]=new TreeNode[1];
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) 
-    {
-      ArrayList<TreeNode> arr=new ArrayList<>();
-      ArrayList<TreeNode> arr1=new ArrayList<>();
-      boolean a[]=new boolean[1];
-      TreeNode k=root;
-      TreeNode f=root;
-      Low(k,arr,p,a);
+    {   
        
-
-      a[0]=false;
-      Low(f,arr1,q,a);
-      int i,j;
-      int n=arr.size();
-      int m=arr1.size();
-      TreeNode l=null;
-      for(i=0;i<n;i++)
-      {
-        for(j=0;j<m;j++)
+        if(root==null)
         {
-            if(arr.get(i)==arr1.get(j))
-            {  
-                l=arr.get(i);
-                
-            }
+            return null;
         }
-      }
-
-return l;
-    }
-    public void Low(TreeNode root,ArrayList<TreeNode> arr,TreeNode p,boolean b[])
-    {
-        if(root==null||b[0]==true)
+        if(root==p)
         {
-            return;
+            return p;
         }
-
+        if(root==q)
+        {
+            return q;
+        }
+        TreeNode l= lowestCommonAncestor( root.left,p, q) ;
+        TreeNode r= lowestCommonAncestor( root.right,p, q);
+        if(l==null&&r!=null)
+        {
+            return r;
+        }
+        else if(l!=null&&r==null)
+        {
+            return l;
+        }
+        else if(l!=null&&r!=null)
+        {
+           
+            return root;
+        }
         
-          arr.add(root);
-        if(p==root)
-        {
-            b[0]=true;
-          return;  
-        }
-        else
-        {
-            
-            if(root.left!=null)
-            {
-                Low(root.left,arr,p,b);
-            }
-            if(root.right!=null)
-            {
-                Low(root.right,arr,p,b);
-            }
-            if(!b[0])
-            arr.remove(arr.size()-1);
-        }
-    }
-}
+
+return null;
+
+     
+}}
