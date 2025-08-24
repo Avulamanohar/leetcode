bdbbe36d230b043952f20b80343a74339a98ff99
Avulamanohar <125465328+Avulamanohar@users.noreply.github.com>
2025-08-24 11:55:09 +0530

    Time: 6 ms (29.16%), Space: 45.5 MB (17.36%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -11,57 +11,19 @@
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
     {
-        ArrayList<TreeNode> arr=new ArrayList<>();
-        ArrayList<TreeNode> arr1=new ArrayList<>();
-        boolean b[]=new boolean[1];
-        LCA(root,p,arr,b);
-        b[0]=false;
-        LCA(root,q,arr1,b);
-       int i,j;
-       TreeNode t=null;
-       
-       for(i=0;i<arr.size();i++)
+       if(root==null||root==p||root==q)
        {
-        for(j=0;j<arr1.size();j++)
-        {
-            if(arr.get(i)==arr1.get(j))
-            {
-             t=arr.get(i);
-            }
-           
-        }
+        return root;
        }
-        return t;
-    
-    }
-    public void LCA(TreeNode root,TreeNode p,List<TreeNode> arr,boolean b[])
-    {    if(b[0]==true)
-    {
-        return;
-    }
-    
-        if(root==null)
-        {
-            return;
-        }
-        arr.add(root);
-        if(root==p)
-        {
-            b[0]=true;
-            return;
-        }
-        
-            if(root.left!=null)
-            {
-                LCA(root.left,p,arr,b);
-            }
-            if(root.right!=null)
-            {
-                LCA(root.right,p,arr,b);
-            }
-            if(!b[0])
-            arr.remove(arr.size()-1);
-    
+        TreeNode l= lowestCommonAncestor(root.left, p,q);
+        TreeNode r= lowestCommonAncestor(root.right,p,q);
+       if(l!=null&&r!=null)
+
+       
+       {
+        return root;
+       }
+       return l!=null?l:r;
         
     }
 }
