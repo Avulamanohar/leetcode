569bc62e0d710837a5037698bb68fa4f9b65a8c0
Avulamanohar <125465328+Avulamanohar@users.noreply.github.com>
2025-11-03 06:09:25 +0530

    Time: 0 ms (100%), Space: 42.3 MB (5.56%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -2,25 +2,40 @@ class Solution {
     public int rob(int[] nums) 
     {  int n=nums.length;
         int dp[]=new int[n];
+         int j=0;
+         if(n==1)
+         {
+            return nums[0];
+         }
+        for(int i=0;i<n;i++)
+        {
+            dp[i]=-1;
+        }
         dp[0]=nums[0];
-        if(n==1)
-        {
-            return dp[0];
-        }
-        dp[1]=nums[1];
-        int b=0;
-        for(int i=2;i<n;i++)
-        {
-            
-            int a=0;
-            for(int j=0;j<i-1;j++)
-            {
-             a=Math.max(nums[i]+dp[j],a);
-            
-            }
-            dp[i]=a;
-             b=Math.max(a,b);
-        }
+        dp[1]=Math.max(nums[1],nums[0]);
+       
+        dpr(n-1,nums,dp);
         return Math.max(dp[n-1],dp[n-2]);
+
+    }
+    public int dpr(int n,int nums[],int dp[])
+    {
+        if(n<0)
+        {
+            return 0;
+        }
+        
+        if(dp[n]!=-1)
+        {
+            return dp[n];
+        }
+        int a=nums[n]+dpr(n-2,nums,dp);
+        int b=dpr(n-1,nums,dp);
+      
+          
+         dp[n]=Math.max(a,b);
+        
+        return dp[n];
+
     }
 }
