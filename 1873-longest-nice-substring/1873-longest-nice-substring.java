14b0475137809bde63f21c43d38659b2aa06ae37
Avulamanohar <125465328+Avulamanohar@users.noreply.github.com>
2025-11-13 19:44:32 +0530

    Time: 18 ms (27.03%), Space: 45.3 MB (16.74%) - LeetHub

--- 1873-longest-nice-substring/1873-longest-nice-substring.java
+++ 1873-longest-nice-substring/1873-longest-nice-substring.java
@@ -8,36 +8,39 @@ class Solution {
         int max=0;
         for(int i=0;i<s.length();i++)
         {
-            Set<Character> up=new HashSet<>();
-            Set<Character> lp=new HashSet<>();
-            Set<Character> ov=new HashSet<>();
-            
-
-           
+            Set<Character> st=new HashSet<>();
+            int d=0;
             for(int j=i;j<s.length();j++)
-            { 
-                int c=(int)s.charAt(j);
-                if(c<b)
-                {
-                    up.add(s.charAt(j));
-                }
-                else
-                {
-                    lp.add(s.charAt(j));
-                }
-                char h=s.charAt(j);
-                ov.add(Character.toLowerCase(h));
-                 
-                 
-                 if(ov.size()==up.size()&&up.size()==lp.size())
+            {
+                 int t=(int)s.charAt(j);
+                 if(t<b)
                  {
-                  if(j-i+1>max)
-                  {
+                    char c=(char)(t+a);
+                    if(st.contains(c)&&!st.contains(s.charAt(j)))
+                    {
+                        d++;
+                    }
+                    st.add(s.charAt(j)); 
+                 }
+                 else
+                 {
+                    char c=(char)(t-a);
+                    if(st.contains(c)&&!st.contains(s.charAt(j)))
+                    {
+                        d++;
+                    }
+                    st.add(s.charAt(j));
+
+                 }
+                 
+                 if(st.size()==d*2)
+                 {
+                    if(j-i+1>max)
+                    {
                     k=i;
                     f=j;
                     max=j-i+1;
-                  }
-                    
+                    }
                     
                  }
                 
