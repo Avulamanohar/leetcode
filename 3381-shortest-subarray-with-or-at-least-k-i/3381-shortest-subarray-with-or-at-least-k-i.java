109360c496a1a2e8611d2171a7d0da6785afff17
Avulamanohar <125465328+Avulamanohar@users.noreply.github.com>
2025-11-14 08:42:03 +0530

    Time: 1 ms (99.48%), Space: 44.3 MB (5.18%) - LeetHub

---
+++ 3381-shortest-subarray-with-or-at-least-k-i/3381-shortest-subarray-with-or-at-least-k-i.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minimumSubarrayLength(int[] nums, int k)
+    {
+        int b=Integer.MAX_VALUE;
+        int n=nums.length;
+        for(int i=0;i<n;i++)
+        {  int f=0;
+            for(int j=i;j<n;j++)
+            {
+             f|=nums[j];
+             if(f>=k)
+             {
+                b=Math.min(b,j-i+1);
+             }
+            }
+        }
+        if(b==Integer.MAX_VALUE)
+        {
+            return -1;
+        }
+        return b;
+        
+    }
+}
