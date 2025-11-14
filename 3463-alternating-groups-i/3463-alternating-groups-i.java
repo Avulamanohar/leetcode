2d8ce0fed2d5296789db23d74866ec6671c2c72a
Avulamanohar <125465328+Avulamanohar@users.noreply.github.com>
2025-11-14 09:28:14 +0530

    Time: 1 ms (98.32%), Space: 44.6 MB (16.07%) - LeetHub

--- 3463-alternating-groups-i/3463-alternating-groups-i.java
+++ 3463-alternating-groups-i/3463-alternating-groups-i.java
@@ -2,42 +2,17 @@ class Solution {
     public int numberOfAlternatingGroups(int[] colors)
      
     {
-        int c=0;
+       int c=0;
         int n=colors.length;
-        String sb="";
-        for(int i=0;i<3;i++)
-        {   if(colors[i]==1)
+        for(int i=0;i<n;i++)
         {
-            sb+="1";
-        }
-            else
+            if(colors[(n+(i-1))%n]!=colors[i]&&colors[(n+(i+1))%n]!=colors[i])
             {
-                sb+="0";
+                c++;
             }
 
-        }
-        if(sb.equals("101")||sb.equals("010"))
-        {
-            c++;
-        }
-        for(int i=3;i<n+2;i++)
-        {
-          sb=sb.substring(1,3);
-           if(colors[i%n]==1)
-        {
-            sb+="1";
-        }
-            else
-            {
-                sb+="0";
-            }
-          if(sb.equals("101")||sb.equals("010"))
-        {
-            c++;
-        }
         }
         return c;
-
         
     }
 }
