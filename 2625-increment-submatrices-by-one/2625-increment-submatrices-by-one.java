348fc37817a1a20117de17089bb3ed950521ff0d
Avulamanohar <125465328+Avulamanohar@users.noreply.github.com>
2025-11-14 08:25:09 +0530

    Time: 455 ms (24.56%), Space: 77.3 MB (16.23%) - LeetHub

---
+++ 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[][] rangeAddQueries(int n, int[][] queries) {
+        int ar[][]=new int[n][n];
+        for(int i=0;i<queries.length;i++)
+        {
+            int a=queries[i][0];
+            int b=queries[i][1];
+            int c=queries[i][2];
+            int d=queries[i][3];
+            for(int j=a;j<=c;j++)
+            {
+                for(int k=b;k<=d;k++)
+                {
+                    ar[j][k]+=1;
+                }
+            }
+            
+        }
+        return ar;
+        
+    }
+}
