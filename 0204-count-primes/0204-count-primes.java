c899dca04e35a22e8a42b5e081bf544c1e9f2514
Avulamanohar <125465328+Avulamanohar@users.noreply.github.com>
2026-03-14 19:08:45 +0530

    Time: 82 ms (95.89%), Space: 51.2 MB (50.73%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int countPrimes(int n)
+    {
+        int c=0;
+        boolean b[]=new boolean[n+1];
+        if(n<2)
+        {
+            return 0;
+        }
+        for(int i=2;i*i<n;i++)
+        {
+            if(!b[i])
+            {
+              
+            for(int j=i*i;j<n;j+=i)
+            {
+                
+                    b[j]=true;
+                
+
+            }}
+        }
+        
+        for(int i=2;i<n;i++)
+        {
+            if(!b[i])
+            {
+                c++;
+            }
+        }
+        return c;
+        
+    }
+}
